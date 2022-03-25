0b1baf3f4bf2849f6c9503f131f72544ba9159e2
admbalogh <baloghadam91@icloud.com>
2022-03-25 20:28:50 +0100

    Code update
    JS,CSS,HTML update

--- homework.jsx
+++ homework.jsx
@@ -1,3 +1,31 @@
+// Digital Clock
+
+setInterval(showTime, 1000);
+function showTime() {
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  am_pm = "AM";
+
+  if (hour > 12) {
+    hour -= 12;
+    am_pm = "PM";
+  }
+  if (hour == 0) {
+    hr = 12;
+    am_pm = "AM";
+  }
+
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  let currentTime = `${hour}:${min}:${sec} ${am_pm}⏰`;
+
+  document.getElementById("clock").innerHTML = currentTime;
+}
+showTime();
 // Challenge 1
 let now = new Date();
 
@@ -43,46 +71,52 @@ if (minute < 10) {
 }
 
 let liveDate = document.querySelector("#live");
-liveDate.innerHTML = `${year}.${currentMonth}.${date}. ${currentDay}`;
+liveDate.innerHTML = `${year}.${currentMonth}.${date}.${currentDay}`;
 
 //Challenge 1
+function displayWeatherConditions(response) {
+  console.log(response);
+  document.querySelector(
+    "#city"
+  ).innerHTML = `${response.data.name},${response.data.sys.country}`;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#windspeed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
 
-function search(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#city");
-  let city = document.querySelector("#city-input");
-  cityInput.innerHTML = `${city.value} `;
+  let weatherIcon = document.querySelector("#icon");
+  weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+}
 
-  function showCity(reaction) {
-    let temperature = Math.round(reaction.data.main.temp);
-    let cityTemperature = document.querySelector("#temperature");
-    cityTemperature.innerHTML = `${temperature}˚C`;
-    let cityName = reaction.data.name;
-    let city = document.querySelector("#city");
-    city.innerHTML = cityName;
-  }
+function searchCity(city) {
   let units = "metric";
   let apiKeycity = "c6d9f7aef9ff8091de9eeba09c596035";
-  let apiUrlcity = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKeycity}&units=${units}`;
+  let apiUrlcity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKeycity}&units=${units}`;
 
-  axios.get(apiUrlcity).then(showCity);
+  axios.get(apiUrlcity).then(displayWeatherConditions);
 }
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
 
+function submitHandle(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", submitHandle);
+
+searchCity("London");
 //Bonus Challenge 2
 
 //API Seach engine
-function showTemp(response) {
-  console.log(response);
-  let myTemp = Math.round(response.data.main.temp);
-  let myPosition = response.data.name;
-  let mylocation = document.querySelector("h1");
-  mylocation.innerHTML = `${myPosition}`;
-
-  let mylocationTemp = document.querySelector("#temperature");
-  mylocationTemp.innerHTML = `${myTemp}˚C`;
-}
 
 function showPosition(position) {
   let lat = position.coords.latitude;
@@ -90,7 +124,7 @@ function showPosition(position) {
   let units = "metric";
   let apiKey = "c6d9f7aef9ff8091de9eeba09c596035";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showTemp);
+  axios.get(apiUrl).then(displayWeatherConditions);
 }
 function getMyPosition() {
   navigator.geolocation.getCurrentPosition(showPosition);
@@ -98,32 +132,3 @@ function getMyPosition() {
 
 let locationButton = document.querySelector("#myLocation");
 locationButton.addEventListener("click", getMyPosition);
-
-// Digital Clock
-
-setInterval(showTime, 1000);
-function showTime() {
-  let time = new Date();
-  let hour = time.getHours();
-  let min = time.getMinutes();
-  let sec = time.getSeconds();
-  am_pm = "AM";
-
-  if (hour > 12) {
-    hour -= 12;
-    am_pm = "PM";
-  }
-  if (hour == 0) {
-    hr = 12;
-    am_pm = "AM";
-  }
-
-  hour = hour < 10 ? "0" + hour : hour;
-  min = min < 10 ? "0" + min : min;
-  sec = sec < 10 ? "0" + sec : sec;
-
-  let currentTime = `${hour}:${min}:${sec} ${am_pm}⏰`;
-
-  document.getElementById("clock").innerHTML = currentTime;
-}
-showTime();
